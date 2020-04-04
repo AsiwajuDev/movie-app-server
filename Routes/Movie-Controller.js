1b310cc1bbb4663c3e6bc637df569ab3732ea2f6
asiwajudev <olanrewajuranda@gmail.com>
2020-04-04 21:46:33 +0100

    Create, Update, Delete, GetMovies, GetMovieById Routes Logic Controller completed

--- Routes/Movie-Controller.js
+++ Routes/Movie-Controller.js
@@ -89,3 +89,34 @@ deleteMovie = async (req,res)=>{
         return res.status(200).json({success: true, data: movie}).catch(err => console.log(err))
     })
 }
+
+getMovieById = async (req, res) => {
+    await Movie.findOne({ _id: req.params.id }, (err, movie) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!movie) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Movie not found` })
+        }
+        return res.status(200).json({ success: true, data: movie })
+    }).catch(err => console.log(err))
+}
+
+getMovies = async (req, res) => {
+    await Movie.find({}, (err, movies) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!movies.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Movie not found` })
+        }
+        return res.status(200).json({ success: true, data: movies })
+    }).catch(err => console.log(err))
+}
+
+module.exports = { createMovie, updateMovie, deleteMovie,getMovies,getMovieById}
